1b31071196500391b7afe033a3b1d3a85084a2b7
Antheas Kapenekakis <git@antheas.dev>
2025-10-25 19:19:31 +0200

    add oci creation

---
+++ oci.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+set -e
+
+FEDORA_VERSION=${FEDORA_VERSION:-43}
+
+# Create buildah from scratch
+BOCI=$(buildah from scratch)
+
+# Trap to remove on errors
+trap 'buildah rm $BOCI' ERR
+
+# Mount the filesystem
+MOCI=$(buildah mount $BOCI)
+
+mkdir -p $MOCI/rpms
+
+# Copy rpms from ./build/RPMS/fVER for each fVER
+for DIR in ./build/RPMS/f${FEDORA_VERSION}/*; do
+    if [ ! -d "$DIR" ]; then
+        continue
+    fi
+
+    # Copy only binary RPMs (exclude src.rpm) directly into rpms/
+    find "$DIR" -type f -name "*.rpm" ! -name "*.src.rpm" -exec cp -t "$MOCI/rpms/" {} +
+done
+
+# Unmount the filesystem
+buildah unmount $BOCI
+
+# Commit the image
+buildah commit $BOCI nvidia_oci_image
+
+# Get digest
+DIGEST=$(buildah images --noheading --format "{{.Digest}}" nvidia_oci_image)
+echo "OCI Image created with digest: $DIGEST"
+
+echo $DIGEST > .oci-digest
