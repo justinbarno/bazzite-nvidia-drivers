fae5812e11ce89c008e4ed7de4c81485beb52193
Antheas Kapenekakis <git@antheas.dev>
2025-11-16 22:24:43 +0100

    put it in the correct place

--- build-nct.sh
+++ build-nct.sh
@@ -16,9 +16,9 @@ git -C ./nvidia-container-toolkit/third_party/libnvidia-container fetch --depth=
 LIB_VERSION=$(git -C ./nvidia-container-toolkit/third_party/libnvidia-container describe --tags --abbrev=0 | sed 's/^v//')
 
 DIST_DIR="$(pwd)/build/NVT" \
-    make -C ./nvidia-container-toolkit/third_party/libnvidia-container centos8-$ARCH
+    make -C ./nvidia-container-toolkit/third_party/libnvidia-container LIB_VERSION=$LIB_VERSION centos8-$ARCH
 LIB_NAME="nct" DIST_DIR="$(pwd)/build/NVT" \
-    make -C ./nvidia-container-toolkit LIB_TAG="" DOCKER=podman LIB_VERSION=$LIB_VERSION centos8-$ARCH
+    make -C ./nvidia-container-toolkit LIB_TAG="" DOCKER=podman centos8-$ARCH
 rm -rf ./build/RPMS/f$FEDORA_VERSION/nvt-$ARCH
 mkdir -p ./build/RPMS/f$FEDORA_VERSION/nvt-$ARCH
 mv build/NVT/centos8/$ARCH/*.rpm ./build/RPMS/f$FEDORA_VERSION/nvt-$ARCH
