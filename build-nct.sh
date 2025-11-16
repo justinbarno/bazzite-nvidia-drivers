ffaab32470b6ac5bc967f04dfe2fb25eb398aa0c
Antheas Kapenekakis <git@antheas.dev>
2025-11-16 22:23:26 +0100

    try a different fix for grabbing tag for major version

--- build-nct.sh
+++ build-nct.sh
@@ -11,14 +11,14 @@ mkdir -p build/NVT/centos8/$ARCH
 export DOCKER="podman"
 export PATH="$(pwd):$PATH"
 
-# Required to grab the tag info from git
-git -C ./nvidia-container-toolkit fetch --depth=200
+# Get previous for libnvidia-container manually
 git -C ./nvidia-container-toolkit/third_party/libnvidia-container fetch --depth=200
+LIB_VERSION=$(git -C ./nvidia-container-toolkit/third_party/libnvidia-container describe --tags --abbrev=0 | sed 's/^v//')
 
 DIST_DIR="$(pwd)/build/NVT" \
     make -C ./nvidia-container-toolkit/third_party/libnvidia-container centos8-$ARCH
 LIB_NAME="nct" DIST_DIR="$(pwd)/build/NVT" \
-    make -C ./nvidia-container-toolkit LIB_TAG="" DOCKER=podman centos8-$ARCH
+    make -C ./nvidia-container-toolkit LIB_TAG="" DOCKER=podman LIB_VERSION=$LIB_VERSION centos8-$ARCH
 rm -rf ./build/RPMS/f$FEDORA_VERSION/nvt-$ARCH
 mkdir -p ./build/RPMS/f$FEDORA_VERSION/nvt-$ARCH
 mv build/NVT/centos8/$ARCH/*.rpm ./build/RPMS/f$FEDORA_VERSION/nvt-$ARCH
