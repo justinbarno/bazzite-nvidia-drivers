796a7faa0b5cad31aa3dd4ca7bb1251660c3cbf1
Antheas Kapenekakis <git@antheas.dev>
2025-11-16 22:18:36 +0100

    switch to proper makefile

--- build-nct.sh
+++ build-nct.sh
@@ -16,7 +16,7 @@ git -C ./nvidia-container-toolkit fetch --depth=200
 git -C ./nvidia-container-toolkit/third_party/libnvidia-container fetch --depth=200
 
 DIST_DIR="$(pwd)/build/NVT" \
-    make -C ./nvidia-container-toolkit/third_party/libnvidia-container -f mk/docker.mk centos8-$ARCH
+    make -C ./nvidia-container-toolkit/third_party/libnvidia-container centos8-$ARCH
 LIB_NAME="nct" DIST_DIR="$(pwd)/build/NVT" \
     make -C ./nvidia-container-toolkit LIB_TAG="" DOCKER=podman centos8-$ARCH
 rm -rf ./build/RPMS/f$FEDORA_VERSION/nvt-$ARCH
